df8012a8a5ca0c6792fdcb276cc2febf78936e66
JustinMoua <Hmuas11037@gmail.com>
2025-05-28 14:00:40 -0500

    Changes to UI and reimplementation of footer.

--- okstateacm.github.io/src/app/page.js
+++ okstateacm.github.io/src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        
+        <h1 className="text-black text-stroke text-4xl text-center">Association for Computing Machinery<br/>at<br/>Oklahoma State University</h1>
         <Image
           className="" 
           src="/ACM Logo (Transparent).png" //location of logo
@@ -13,12 +13,19 @@ export default function Home() {
           height={38}
           priority
         />
-        <h1 className="text-black">test</h1>
+        <h1 className="text-black text-stroke text-md text-center">Welcome to Oklahoma State University's<br/>Computer Science community!</h1>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full py-3 px-6 border border-solid bg-black"
-            href="https://www.youtube.com/" //Link to somewhere.
+            href="" //Link to somewhere.
+            target="_self" //_self opens link in the same frame. _blank opens in a new tab/window. Note that it is already _self by default. Putting this here for emphasis.
+          >
+            Officers
+          </a>
+          <a
+            className="rounded-full py-3 px-6 border border-solid bg-black"
+            href="" //Link to somewhere.
             target="_self" //_self opens link in the same frame. _blank opens in a new tab/window. Note that it is already _self by default. Putting this here for emphasis.
           >
             About Us
@@ -32,7 +39,7 @@ export default function Home() {
           </a>
           <a
             className="rounded-full py-3 px-6 border border-solid bg-black"
-            href="https://www.youtube.com/" //Link to somewhere.
+            href="" //Link to somewhere.
             target="_self" //_self opens link in the same frame. _blank opens in a new tab/window. Note that it is already _self by default. Putting this here for emphasis.
           >
             Our History
@@ -40,8 +47,8 @@ export default function Home() {
 
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        {/* <a
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center bg-color:orange">
+        <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -49,12 +56,12 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/file.svg" //ADD AN EMAIL ICON HERE
             alt="File icon"
             width={16}
             height={16}
           />
-          Learn
+          acm.cs@okstate.edu
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -64,12 +71,12 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/window.svg" 
             alt="Window icon"
             width={16}
             height={16}
           />
-          Examples
+          PLACEHOLDER FOOTER
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -84,8 +91,8 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
-        </a> */}
+          PLACEHOLDER FOOTER
+        </a>
       </footer>
     </div>
   );
