65718f0465d5466cb2219e26f6a0b655ea0d9122
Justin Moua <39628304+JustinMoua@users.noreply.github.com>
2025-05-27 19:33:39 -0500

    Changes to buttons and UI appearance.

---
+++ README.md
@@ -0,0 +1,19 @@
+Welcome! This is the GitHub repository for Oklahoma State University's Association for Computing Machinery chapter's website. 
+</br>
+This was last edited by Justin Moua on 5/27/2025.
+
+# Pre-Requisites (Work in Progress)
+This project uses the Next.js React framework. You will want to ensure you have [Node.js 18.18][Node.js Link] or later.
+
+Once you have cloned the repository:
+1. Open up your device's terminal.
+2. Go to the \okstateacm.github.io folder within this repository.
+3. Type ```npm install next``` into the terminal.
+
+
+# How do I run this project?
+1. Open up your device's terminal.
+2. Go to the \okstateacm.github.io folder within this repository.
+3. Type ```npm run dev``` into the terminal. You should see a localhost and network http link. Click on one of them, copy and paste or type one of them into a browser.
+
+[Node.js Link]: https://nodejs.org/en
--- okstateacm.github.io/src/app/page.js
+++ okstateacm.github.io/src/app/page.js
@@ -3,46 +3,45 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        
         <Image
-          className="dark:invert" 
-          src="/next.svg" //location of logo
+          className="" 
+          src="/ACM Logo (Transparent).png" //location of logo
           alt="Next.js logo" //alt text of file
           width={180}
           height={38}
           priority
         />
+        <h1 className="text-black">test</h1>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          {/* Upcoming Events Button */}
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="rounded-full py-3 px-6 border border-solid bg-black"
+            href="https://www.youtube.com/" //Link to somewhere.
+            target="_self" //_self opens link in the same frame. _blank opens in a new tab/window. Note that it is already _self by default. Putting this here for emphasis.
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
+            About Us
+          </a>
+          <a
+            className="rounded-full py-3 px-6 border border-solid bg-black"
+            href="https://www.youtube.com/" //Link to somewhere.
+            target="_self" //_self opens link in the same frame. _blank opens in a new tab/window. Note that it is already _self by default. Putting this here for emphasis.
+          >
             Upcoming Events
           </a>
-          {/* Contact us Button */}
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="rounded-full py-3 px-6 border border-solid bg-black"
+            href="https://www.youtube.com/" //Link to somewhere.
+            target="_self" //_self opens link in the same frame. _blank opens in a new tab/window. Note that it is already _self by default. Putting this here for emphasis.
           >
-            Contact Us!
+            Our History
           </a>
+
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -86,7 +85,7 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>
+        </a> */}
       </footer>
     </div>
   );
