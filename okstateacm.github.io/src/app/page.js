a8e4152969f818791f702edd5d3e526d50f53f02
JustinMoua <Hmuas11037@gmail.com>
2025-05-28 19:08:14 -0500

    UI changes

--- okstateacm.github.io/src/app/page.js
+++ okstateacm.github.io/src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        <h1 className="text-black text-stroke text-4xl text-center">Association for Computing Machinery<br/>at<br/>Oklahoma State University</h1>
+        <h1 className="text-white text-stroke text-5xl text-center">Association for Computing Machinery<br/>at<br/>Oklahoma State University</h1>
         <Image
           className="" 
           src="/ACM Logo (Transparent).png" //location of logo
@@ -13,7 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <h1 className="text-black text-stroke text-md text-center">Welcome to Oklahoma State University's<br/>Computer Science community!</h1>
+        <h1 className="text-white text-3xl text-center">Welcome to Oklahoma State University's<br/>Computer Science community!</h1>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
