4067142eb793c1427ab2e96fbe8fd8bf01d37452
Justin Moua <39628304+JustinMoua@users.noreply.github.com>
2025-06-15 00:29:01 -0500

    Added officers page. Made changes to UI on home page.

---
+++ okstateacm.github.io/src/app/officers/page.js
@@ -0,0 +1,205 @@
+import Image from "next/image";
+
+
+//* AUTHORS: Justin Moua
+//*
+//*
+//*
+
+// This is called from page.js from src\app.
+export default function Officers() {
+  return (
+      <div className="flex flex-col min-h-screen">
+          
+      <header className="w-full bg-black text-white py-4 px-8 flex items-center">
+        <a
+          className="rounded-full py-3 px-6 border border-solid bg-black"
+          href="/"
+          target="_self"
+        >
+          Home
+        </a>
+        <a
+          className="rounded-full py-3 px-6 border border-solid bg-black"
+          href="/"
+          target="_self"
+        >
+          About Us
+        </a>
+        <a
+          className="rounded-full py-3 px-6 border border-solid bg-black"
+          href="/calendar"
+          target="_self"
+        >
+          Upcoming Events
+        </a>
+        
+        {/* <h1 className="text-2xl font-bold">ACM Officers</h1> */}
+      </header>
+
+      
+      <main className="flex-1 overflow-y-auto p-8 ">
+        <div className="max-w-3xl mx-auto space-y-6">
+            <div className="bg-white rounded-lg shadow p-6">
+                <div className="flex items-center justify-between">
+                    {/* Left: Image and text */}
+                    <div className="flex items-center gap-4">
+                    <img
+                        src="/silhouette.png"
+                        alt="Profile"
+                        className="w-12 h-12 rounded-full object-cover"
+                    />
+                    <div>
+                        <h2 className="text-black text-xl font-semibold">President</h2>
+                        <span className="text-black">Ricardo Mulino</span>
+                    </div>
+                    </div>
+                    {/* Right: LinkedIn icon */}
+                    <a
+                    href="https://www.linkedin.com/in/ricardo-mulino-2093052b6/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+
+                    <Image
+                    aria-hidden
+                    src="/linkedin.png" //ADD AN EMAIL ICON HERE
+                    alt="File icon"
+                    width={80}
+                    height={0}
+                    />
+                    {/* don't need to do the following. */}
+                    {/* <img src="/linkedin.png" alt="LinkedIn" className="w-20 h-5" /> */}
+                    </a>
+                </div>
+            </div>
+
+            <div className="bg-white rounded-lg shadow p-6">
+                <div className="flex items-center justify-between">
+                    {/* Left: Image and text */}
+                    <div className="flex items-center gap-4">
+                    <img
+                        src="/silhouette.png"
+                        alt="Profile"
+                        className="w-12 h-12 rounded-full object-cover"
+                    />
+                    <div>
+                        <h2 className="text-black text-xl font-semibold">Vice President</h2>
+                        <span className="text-black">Savannah Hamblin</span>
+                    </div>
+                    </div>
+                    {/* Right: LinkedIn icon */}
+                    <a
+                    href="https://www.linkedin.com/in/savannah-hamblin-70927b237/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+
+                    <Image
+                    aria-hidden
+                    src="/linkedin.png" //ADD AN EMAIL ICON HERE
+                    alt="File icon"
+                    width={80}
+                    height={0}
+                    />
+                    {/* don't need to do the following. */}
+                    {/* <img src="/linkedin.png" alt="LinkedIn" className="w-20 h-5" /> */}
+                    </a>
+                </div>
+            </div>
+                  
+            <div className="bg-white rounded-lg shadow p-6">
+                <div className="flex items-center justify-between">
+                    {/* Left: Image and text */}
+                    <div className="flex items-center gap-4">
+                    <img
+                        src="/silhouette.png"
+                        alt="Profile"
+                        className="w-12 h-12 rounded-full object-cover"
+                    />
+                    <div>
+                        <h2 className="text-black text-xl font-semibold">Treasurer</h2>
+                        <span className="text-black">Ethan Franks</span>
+                    </div>
+                    </div>
+                    {/* Right: LinkedIn icon */}
+                    <a
+                    href="https://www.linkedin.com/in/ethan-franks-377634272/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                        
+                    <Image
+                    aria-hidden
+                    src="/linkedin.png" //ADD AN EMAIL ICON HERE
+                    alt="File icon"
+                    width={80}
+                    height={0}
+                    />
+                    {/* don't need to do the following. */}
+                    {/* <img src="/linkedin.png" alt="LinkedIn" className="w-20 h-5" /> */}
+                    </a>
+                </div>
+            </div>
+                  
+            <div className="bg-white rounded-lg shadow p-6">
+                <div className="flex items-center justify-between">
+                    {/* Left: Image and text */}
+                    <div className="flex items-center gap-4">
+                    <img
+                        src="/silhouette.png"
+                        alt="Profile"
+                        className="w-12 h-12 rounded-full object-cover"
+                    />
+                    <div>
+                        <h2 className="text-black text-xl font-semibold">Secretary</h2>
+                        <span className="text-black">Jack Weinheimer</span>
+                    </div>
+                    </div>
+                    {/* Right: LinkedIn icon */}
+                    <a
+                    href="https://www.linkedin.com/in/jack-weinheimer-96469518a/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                          >
+                    <Image
+                    aria-hidden
+                    src="/linkedin.png" //ADD AN EMAIL ICON HERE
+                    alt="File icon"
+                    width={80}
+                    height={0}
+                    />
+                    {/* don't need to do the following. */}
+                    {/* <img src="/linkedin.png" alt="LinkedIn" className="w-20 h-5" /> */}
+                    </a>
+                </div>
+            </div>
+                  
+        </div>
+      </main>
+
+
+      <footer className="w-full bg-black text-white py-4 px-8 flex items-center justify-center">
+        <div className="flex gap-10">
+
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="mailto:acm.cs@okstate.edu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/missing_textures2.png" //ADD AN EMAIL ICON HERE
+              alt="Email Icon"
+              width={16}
+              height={16}
+            />
+            acm.cs@okstate.edu
+          </a>
+
+        </div>        
+      </footer>
+    </div>
+  );
+}
--- okstateacm.github.io/src/app/page.js
+++ okstateacm.github.io/src/app/page.js
@@ -2,9 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center h-screen p-8 gap-16 sm:p-20 font-Arial">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        <h1 className="text-white text-stroke text-5xl text-center">Association for Computing Machinery<br/>at<br/>Oklahoma State University</h1>
+        <div className="inline-block px-6 py-2 rounded-lg backdrop-blur-xl bg-gray-500/50 border border-orange-500">
+        {/* <div className="inline-block px-6 py-2 rounded-lg backdrop-blur-xl bg-gray-500/50"> */}
+          <h1 className="text-white text-stroke text-5xl text-center">
+            Association <br/>for<br/>Computing Machinery
+          </h1>
+        </div>
         <Image
           className="" 
           src="/ACM Logo (Transparent).png" //location of logo
@@ -13,12 +18,13 @@ export default function Home() {
           height={38}
           priority
         />
-        <h1 className="text-white text-3xl text-center">Welcome to Oklahoma State University's<br/>Computer Science community!</h1>
-
+        <div className="inline-block px-6 py-2 rounded-lg backdrop-blur-xl bg-gray-500/50 border border-orange-500">
+          <h1 className="text-white text-stroke2 text-3xl text-center">Welcome to Oklahoma State University's<br/>Computer Science community!</h1>
+        </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full py-3 px-6 border border-solid bg-black"
-            href="" //Link to somewhere.
+            href="officers" //Link to somewhere.
             target="_self" //_self opens link in the same frame. _blank opens in a new tab/window. Note that it is already _self by default. Putting this here for emphasis.
           >
             Officers
@@ -48,51 +54,39 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center bg-color:orange">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg" //ADD AN EMAIL ICON HERE
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          acm.cs@okstate.edu
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg" 
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          PLACEHOLDER FOOTER
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          PLACEHOLDER FOOTER
-        </a>
+        <div className="flex gap-4 px-6 py-1 rounded-lg backdrop-blur-xl bg-gray-500/50 border border-orange-500">
+
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://www.instagram.com/okstate.acm/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/window.svg" 
+              alt="Window icon"
+              width={16}
+              height={16}
+            />
+            ACM Instagram
+          </a>
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/globe.svg"
+              alt="Globe icon"
+              width={16}
+              height={16}
+            />
+            PLACEHOLDER FOOTER
+          </a>
+        </div>
       </footer>
     </div>
   );
